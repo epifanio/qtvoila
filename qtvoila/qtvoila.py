ab7cc005b9230ea0511856e2737727123da9be9b
Massimo Di Stefano <epiesasha@me.com>
2022-08-06 21:01:00 +0200

    Update qtvoila.py

--- qtvoila/qtvoila.py
+++ qtvoila/qtvoila.py
@@ -1,5 +1,5 @@
-from PySide2 import QtCore
-from PySide2.QtWebEngineWidgets import QWebEngineView
+from PyQt5 import QtCore
+from PyQt5.QtWebEngineWidgets import QWebEngineView
 import nbformat as nbf
 import numpy as np
 import tempfile
